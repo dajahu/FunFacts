33e3f59c82258666cdd0c096a9f7574ee1cd030f
David Jansson Hultgren <davloop@MacBook-Pro.local>
2015-05-18 18:36:21 +0200

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by David Jansson Hultgren on 2015-05-18.
+//  Copyright (c) 2015 davloop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
